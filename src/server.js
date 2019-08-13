769e361936a3085018134a5150d334c2eb0b5fdd
vietanhk-0863 <kieu.viet.anh@sun-asterisk.com>
2019-08-13 17:21:06 +0700

    [1.0.0] Add static pages.

--- src/server.js
+++ src/server.js
@@ -1,22 +1,22 @@
 import express from "express";
 import ConnectDB from "./config/connectDB";
-import ContactModel from "./models/contact.model";
+import configViewEngine from "./config/viewEngine";
+
+// Init app
 let app = express();
 
 //Connect to MongoDb
 ConnectDB();
 
-app.get("/test-database", async (req, res) => {
-  try {
-    let item = {
-      userId: "53245454",
-      contactId: "2345432545"
-    };
-    let contact = await ContactModel.createNew(item);
-    res.send(contact);
-  } catch (error) {
-    console.log(error);
-  }
+// Config view engine
+configViewEngine(app);
+
+app.get("/", (req, res) => {
+  return res.render("main/master");
+});
+
+app.get("/login-register", (req, res) => {
+  return res.render("auth/loginRegister");
 });
 
 app.listen(process.env.APP_PORT, process.env.APP_HOST, () => {
