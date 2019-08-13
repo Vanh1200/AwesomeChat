85ab0a01f66154ec68012f5e9c695bfe060ead3d
vietanhk-0863 <kieu.viet.anh@sun-asterisk.com>
2019-08-13 17:56:12 +0700

    [1.0.0] add routes and controllers

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import ConnectDB from "./config/connectDB";
 import configViewEngine from "./config/viewEngine";
+import initRoutes from "./routes/web";
 
 // Init app
 let app = express();
@@ -11,13 +12,8 @@ ConnectDB();
 // Config view engine
 configViewEngine(app);
 
-app.get("/", (req, res) => {
-  return res.render("main/master");
-});
-
-app.get("/login-register", (req, res) => {
-  return res.render("auth/loginRegister");
-});
+// Init routes
+initRoutes(app);
 
 app.listen(process.env.APP_PORT, process.env.APP_HOST, () => {
   console.log(`Hello Vanh1200 I'm running at ${process.env.APP_HOST}:${process.env.APP_PORT}/`);
