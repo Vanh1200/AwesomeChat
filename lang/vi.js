a6f7a4201beab800f0fe853d64de1b9c4fe1d724
vietanhk-0863 <kieu.viet.anh@sun-asterisk.com>
2019-08-15 01:20:39 +0700

    [1.0.0] register part 2

--- lang/vi.js
+++ lang/vi.js
@@ -4,3 +4,15 @@ export const transValidation = {
   password_confirmation_incorrect: "Nhập lại mật khẩu chưa chính xác!",
   gender_incorrect: "Đừng làm như thế nhá người ae!"
 };
+
+export const transErrors = {
+  account_in_use: "Email này đã được sử dụng.",
+  account_removed: "Tài khoản này đã bị gỡ khỏi hệ thống, nếu tin điều này là hiểu nhầm, vui lòng liên hệ lại với chúng tôi tại hòm thư awesomechat@gmail.com.",
+  account_not_active: "Email này đã được đăng ký nhưng chưa active tìa khoản, vui lòng kiểm tra email của bạn."
+};
+
+export const transSuccess = {
+  userCreated: (userEmail) => {
+    return `Tài khoản <strong>${userEmail}</strong> đã được tạo, vui lòng kiểm tra email để kích hoạt.`
+  }
+};
--- src/server.js
+++ src/server.js
@@ -3,18 +3,26 @@ import ConnectDB from "./config/connectDB";
 import configViewEngine from "./config/viewEngine";
 import initRoutes from "./routes/web";
 import bodyParser from "body-parser";
+import connectFlash from "connect-flash";
+import configSesion from "./config/session";
 
 // Init app
 let app = express();
 
-//Connect to MongoDb
+// Connect to MongoDb
 ConnectDB();
+
+// Config session
+configSesion(app);
 
 // Config view engine
 configViewEngine(app);
 
 // Enable post data for request
 app.use(bodyParser.urlencoded({extended: true}));
+
+// Enable flash message
+app.use(connectFlash());
 
 // Init routes
 initRoutes(app);
