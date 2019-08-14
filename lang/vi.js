6e8fb0085c6f9ee25edfed2e174518fe52c3a8f5
vietanhk-0863 <kieu.viet.anh@sun-asterisk.com>
2019-08-14 17:19:46 +0700

    [1.0.0] register part 1

---
+++ lang/vi.js
@@ -0,0 +1,6 @@
+export const transValidation = {
+  email_incorrect: "Email phải có dạng example@gmail.com!",
+  password_incorrect: "Mật khẩu ít nhất 8 kí tự, bao gồm chữ hoa, chữ thường, chữ số và kí tự đặc biệt!",
+  password_confirmation_incorrect: "Nhập lại mật khẩu chưa chính xác!",
+  gender_incorrect: "Đừng làm như thế nhá người ae!"
+};
