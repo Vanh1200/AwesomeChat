cfc348c9fb9b05d916be4bfd618c2f616d52e06f
vietanhk-0863 <kieu.viet.anh@sun-asterisk.com>
2019-10-10 23:39:50 +0700

    fix

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -1,23 +1,11 @@
 import CommentModel from "../models/commentModel";
-import CommentGroupModel from "../models/commentModel";
 
 let postComment = async (req, res) => {
   try {
     let id = req.body.id;
     let email = req.body.email;
     let user = await UserModel.findByEmail(email);
-    console.log(user);
-    if (!user) {
-      return res.status(401).send({ success: false, msg: 'Authentication failed. User not found.' });
-    }
-    if (!user.local.isActive) {
-      return res.status(401).send({ success: false, msg: 'Authentication failed. User not active.' });
-    }
-
-    let checkPassword = await user.comparePassword(password);
-    if (!checkPassword) {
-      return res.status(401).send({ success: false, msg: 'Authentication failed. Password failed.' });
-    }
+    
     return res.status(200).send(user);
 
   } catch (error) {
@@ -26,4 +14,15 @@ let postComment = async (req, res) => {
   }
 }
 
-module.exports = 
+let getComments = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
+
+module.exports = {
+  postComment: postComment,
+  getComments: getComments
+};
